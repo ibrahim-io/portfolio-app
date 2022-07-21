902bdd2d4341f8c15ff451c15de879caddb7adb8
Ibrahim Ibrahim <ii83326@gmail.com>
2022-07-21 18:58:17 +0100

    feat: Added command line styling.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 
 const App = () => {
+  const introduction = [ '> Hello, My Name is Ibrahim',
+                '> I’m currently a studying Computer Science at Imperial College London',
+                '> I’ve worked with a lot of languages over the years and here is a showcase of ...',
+                '> Things I have learnt',
+                '> Type ‘next’ in the command line to continue....']
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <p>{introduction[0]}</p>
+    </div>
   );
 }
 
